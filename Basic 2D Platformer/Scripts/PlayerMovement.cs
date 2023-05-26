c5766ba4633a06ad62345c83664a7fbfc4f04504
TheZombieGoat <98498075+TheZombieGoat@users.noreply.github.com>
2023-05-26 02:05:34 -0400

    Implemented jumping and added animations

--- Basic 2D Platformer/Scripts/PlayerMovement.cs
+++ Basic 2D Platformer/Scripts/PlayerMovement.cs
@@ -4,23 +4,56 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D body;
     [SerializeField] private float speed;
+    private Animator anim;
+    private bool grounded;
   
     //runs once at start
     private void Awake()
     {
-        //Gets Body
+        //Grabs references for body from Rigidbody and Animator for game object
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        //setting default speed
+        speed = 5;
     }
     
     //runs every frame
     private void Update()
-    {
-       
+    {   
+        float horizontal_input = Input.GetAxis("Horizontal");
+
         //setting up velocity (For x-axis)
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+        body.velocity = new Vector2(horizontal_input * speed, body.velocity.y);
+
+        //flips player when moving left-right
+        if (horizontal_input > 0.01f)
+            transform.localScale = Vector3.one;
+        else if (horizontal_input < -0.01f)
+            transform.localScale = new Vector3(-1,1,1);
 
         //implement Jumping
-        if (Input.GetKey(KeyCode.Space))
-            body.velocity = new Vector2(body.velocity.x, speed);
+        if (Input.GetKey(KeyCode.Space) && grounded)
+            Jump();
+
+        //setting up animator parameters
+        anim.SetBool("Running", horizontal_input != 0);
+        anim.SetBool("grounded", grounded);
     }
+
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, speed);
+        anim.SetTrigger("jump");
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            grounded = true;
+        }
+    }
+
+
 }
