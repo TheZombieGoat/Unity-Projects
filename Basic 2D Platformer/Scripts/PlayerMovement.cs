8078aa23485df128ab4ce23af010ea57c076495f
TheZombieGoat <98498075+TheZombieGoat@users.noreply.github.com>
2023-05-26 21:30:57 -0400

    improved collision physics using boxcasting, added function isGrounded().

--- Basic 2D Platformer/Scripts/PlayerMovement.cs
+++ Basic 2D Platformer/Scripts/PlayerMovement.cs
@@ -4,17 +4,19 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D body;
     [SerializeField] private float speed;
+    [SerializeField] private LayerMask groundLayer;
     private Animator anim;
-    private bool grounded;
+    private BoxCollider2D boxCollider;
   
     //runs once at start
     private void Awake()
     {
-        //Grabs references for body from Rigidbody and Animator for game object
+        //Grabs references for body from Rigidbody, BoxCollider and Animator for game object
         body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
         //setting default speed
-        speed = 5;
+        speed = 10;
     }
     
     //runs every frame
@@ -32,28 +34,27 @@ public class PlayerMovement : MonoBehaviour
             transform.localScale = new Vector3(-1,1,1);
 
         //implement Jumping
-        if (Input.GetKey(KeyCode.Space) && grounded)
+        if (Input.GetKey(KeyCode.Space) && isGrounded())
             Jump();
 
         //setting up animator parameters
         anim.SetBool("Running", horizontal_input != 0);
-        anim.SetBool("grounded", grounded);
+        anim.SetBool("grounded", isGrounded());
+
     }
 
     private void Jump()
     {
         body.velocity = new Vector2(body.velocity.x, speed);
         anim.SetTrigger("jump");
-        grounded = false;
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+
+    private bool isGrounded()
     {
-        if (collision.gameObject.tag == "Ground")
-        {
-            grounded = true;
-        }
+        //using box collision to check if player is on ground.
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center,boxCollider.bounds.size,0,Vector2.down,0.1f,groundLayer);
+        return raycastHit.collider != null;
     }
-
 
 }
