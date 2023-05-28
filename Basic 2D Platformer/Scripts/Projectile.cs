356c53a5a428ddc1caec2aba30c1fb9c0d9ca574
TheZombieGoat <98498075+TheZombieGoat@users.noreply.github.com>
2023-05-27 22:25:10 -0400

    Add files via upload

---
+++ Basic 2D Platformer/Scripts/Projectile.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private BoxCollider2D boxCollider;
+    private Animator anim;
+
+    private bool hit;
+    private float direction;
+    private float lifetime;
+
+    private void Awake()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+        anim = GetComponent<Animator>();
+        speed = 5;
+    }
+    private void Update()
+    {
+        if (hit) return;
+        float movementSpeed = Time.deltaTime * speed * direction;
+        transform.Translate(movementSpeed, 0, 0);
+
+        lifetime += Time.deltaTime;
+        if (lifetime > 5) gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        hit = true;
+        boxCollider.enabled = false;
+        anim.SetTrigger("explode");
+    }
+
+    public void setDirection(float _direction)
+    {
+        lifetime = 0;
+        direction = _direction;
+        gameObject.SetActive(true);
+        hit = false;
+        boxCollider.enabled = true;
+
+        float localScaleX = transform.localScale.x;
+        if (Mathf.Sign(localScaleX) != direction)
+            localScaleX = -localScaleX;
+
+        transform.localScale = new Vector3(localScaleX, transform.localScale.y, transform.localScale.z);
+    }
+
+    private void Deactivate()
+    {
+        gameObject.SetActive(false);
+    }
+}
